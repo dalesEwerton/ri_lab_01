8099ce6481dca6f4b894653f884080515e94d21f
dalesEwerton <dales.fragoso@ccc.ufcg.edu.br>
2019-04-03 19:46:14 -0300

    Nav to more links

--- ri_lab_01/spiders/diario_do_centro_do_mundo.py
+++ ri_lab_01/spiders/diario_do_centro_do_mundo.py
@@ -12,15 +12,11 @@ class DiarioDoCentroDoMundoSpider(scrapy.Spider):
     allowed_domains = ['diariodocentrodomundo.com.br']
     start_urls = []
     id = 1
-
-    def __init__(self, *a, **kw):
-        super(DiarioDoCentroDoMundoSpider, self).__init__(*a, **kw)
-        with open('seeds/diario_do_centro_do_mundo.json') as json_file:
-            data = json.load(json_file)
-        self.start_urls = list(data.values())
+    sections = ['Brasil', 'Politica', 'Mundo', 'Comportamento', 'Economia', 'Esporte', 'Cultura', 'Destaques', 'analise', 'aovivo', 'Humor', 'Religião']
 
     def parse(self, response):
         links = []
+        newLinks = []
         if response.url in self.start_urls:
             # NAV
             links = response.css('h3.td-module-title a::attr(href)').getall()
@@ -28,12 +24,34 @@ class DiarioDoCentroDoMundoSpider(scrapy.Spider):
                 yield response.follow(link, callback=self.parse)
         else:
             # PARSE
-            i = RiLab01Item()
-            i['_id'] = self.id
-            i['author'] = response.css('div.td-post-author-name a::text').get()
-            i['date'] = response.css('span.td-post-date time::attr(datetime)').get()
-            i['title'] = response.css('title::text').get()
-            i['text'] = response.css('p.p1 span.s1::text').getall()
-            i['url'] = response.url
-            self.id = self.id + 1
-            yield i
+            item = RiLab01Item()
+            item['_id']       = self.id
+            item['author']    = response.css('div.td-post-author-name a::text').get()
+            item['date']      = response.css('span.td-post-date time::attr(datetime)').get()
+            item['title']     = response.css('title::text').get()
+            item['sub_title'] = 'N/A'
+            item['section']   = self.getSection(response)
+            item['text']      = self.getText(response)
+            item['url']       = response.url
+            self.id           = self.id + 1
+            yield item
+            if self.id < 200:
+				# NAV
+                link = response.css('div.td-post-next-prev-content a::attr(href)').get() 
+                yield response.follow(link, callback=self.parse)
+            
+            
+    def getText(self, response):
+        textlist = response.css('p.p1 span.s1::text, p::text').getall()
+        finalTextList = []
+        for t in textlist:
+            if 'O jornalismo do DCM precisa de você' not in t:
+                finalTextList.append(t)
+        return finalTextList
+        
+    def getSection(self, response):
+        contentList = response.css('meta::attr(content)').getall()
+        for content in contentList:
+            if content in self.sections:
+                return content
+        return 'N/A'
