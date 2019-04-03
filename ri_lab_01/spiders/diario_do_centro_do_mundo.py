32d4c32e8f87a9c8e4a6cf0c476637981c23ff76
dalesEwerton <dales.fragoso@ccc.ufcg.edu.br>
2019-04-03 20:06:58 -0300

    #Fix init and upload output

--- ri_lab_01/spiders/diario_do_centro_do_mundo.py
+++ ri_lab_01/spiders/diario_do_centro_do_mundo.py
@@ -6,13 +6,19 @@ from ri_lab_01.items import RiLab01Item
 from ri_lab_01.items import RiLab01CommentItem
 
 
-
 class DiarioDoCentroDoMundoSpider(scrapy.Spider):
     name = 'diario_do_centro_do_mundo'
     allowed_domains = ['diariodocentrodomundo.com.br']
     start_urls = []
     id = 1
-    sections = ['Brasil', 'Politica', 'Mundo', 'Comportamento', 'Economia', 'Esporte', 'Cultura', 'Destaques', 'analise', 'aovivo', 'Humor', 'Religião']
+    sections = ['Brasil', 'Politica', 'Mundo', 'Comportamento', 'Economia', 'Esporte', 'Cultura', 'Destaques',
+                'analise', 'aovivo', 'Humor', 'Religião']
+
+    def __init__(self, *a, **kw):
+        super(DiarioDoCentroDoMundoSpider, self).__init__(*a, **kw)
+        with open('seeds/diario_do_centro_do_mundo.json') as json_file:
+            data = json.load(json_file)
+        self.start_urls = list(data.values())
 
     def parse(self, response):
         links = []
@@ -25,22 +31,21 @@ class DiarioDoCentroDoMundoSpider(scrapy.Spider):
         else:
             # PARSE
             item = RiLab01Item()
-            item['_id']       = self.id
-            item['author']    = response.css('div.td-post-author-name a::text').get()
-            item['date']      = response.css('span.td-post-date time::attr(datetime)').get()
-            item['title']     = response.css('title::text').get()
+            item['_id'] = self.id
+            item['author'] = response.css('div.td-post-author-name a::text').get()
+            item['date'] = response.css('span.td-post-date time::attr(datetime)').get()
+            item['title'] = response.css('title::text').get()
             item['sub_title'] = 'N/A'
-            item['section']   = self.getSection(response)
-            item['text']      = self.getText(response)
-            item['url']       = response.url
-            self.id           = self.id + 1
+            item['section'] = self.getSection(response)
+            item['text'] = self.getText(response)
+            item['url'] = response.url
+            self.id = self.id + 1
             yield item
             if self.id < 200:
-				# NAV
-                link = response.css('div.td-post-next-prev-content a::attr(href)').get() 
+                # NAV
+                link = response.css('div.td-post-next-prev-content a::attr(href)').get()
                 yield response.follow(link, callback=self.parse)
-            
-            
+
     def getText(self, response):
         textlist = response.css('p.p1 span.s1::text, p::text').getall()
         finalTextList = []
@@ -48,7 +53,7 @@ class DiarioDoCentroDoMundoSpider(scrapy.Spider):
             if 'O jornalismo do DCM precisa de você' not in t:
                 finalTextList.append(t)
         return finalTextList
-        
+
     def getSection(self, response):
         contentList = response.css('meta::attr(content)').getall()
         for content in contentList:
